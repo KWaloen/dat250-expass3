6ddc22feaf48cf2155b7ae7685c964fee478cba7
KWaloen <kai.waloen@gmail.com>
2025-09-12 18:01:15 +0200

    poll component

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <div className="bg-black flex items-center justify-center h-screen">
 
-      <div className="flex bg-white items-center justify-center p-100">
+      <div className="flex bg-white items-center justify-center p-10">
         <div className="text-black text-center">
           Poll machine!
-          <div>
+          <div className="p-5">
             <Poll />
           </div>
           <div>
--- components/Poll.jsx
+++ components/Poll.jsx
@@ -1,5 +1,33 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
 export default function Poll() {
     return (
-        <div>Test</div>
+        <div>
+            <div>
+                Enter question:
+            </div>
+
+            <Input />
+
+            <div>
+                <div className="pt-5">
+                    Option 1: <Input />
+                </div>
+
+                <div className="pt-5">
+                    Option 2: <Input />
+                </div>
+
+                <div className="pt-5">
+                    Option 3: <Input />
+                </div>
+
+                <div className="pt-5">
+                    <Button>Submit Poll</Button>
+                </div>
+            </div>
+        </div>
+
     )
 }
