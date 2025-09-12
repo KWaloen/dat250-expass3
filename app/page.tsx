6207a35e7747aa026b0bba3d6c7adb41fe36b1cd
KWaloen <kai.waloen@gmail.com>
2025-09-12 18:42:28 +0200

    poll machine

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="bg-black flex items-center justify-center h-screen">
 
-      <div className="flex bg-white items-center justify-center p-10">
+      <div className="flex bg-white items-center p-10">
         <div className="text-black text-center">
           Poll machine!
           <div className="p-5">
--- components/Poll.jsx
+++ components/Poll.jsx
@@ -1,31 +1,72 @@
+"use client"
+
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useState } from "react"
 
 export default function Poll() {
+
+
+    const [question, setQuestion] = useState("");
+    const [option1, setOption1] = useState("");
+    const [option2, setOption2] = useState("");
+    const [option3, setOption3] = useState("");
+
+    const handleClick = () => {
+        setQuestion(question);
+        setOption1(option1);
+        setOption2(option2);
+        setOption3(option3);
+        
+       
+    }
+ 
+    
+
+    
+
     return (
+
         <div>
             <div>
-                Enter question:
+                Enter Question:
+                <Input
+                    value={question}
+                    onChange={(question) => setQuestion(question.target.value)}
+                />
             </div>
-
-            <Input />
 
             <div>
                 <div className="pt-5">
-                    Option 1: <Input />
+                    Option 1:
+                    <Input
+                        value={option1}
+                        onChange={(option1) => setOption1(option1.target.value)}
+                    />
                 </div>
 
                 <div className="pt-5">
-                    Option 2: <Input />
+                    Option 2:
+                    <Input
+                        value={option2}
+                        onChange={(option2) => setOption2(option2.target.value)}
+                    />
                 </div>
 
                 <div className="pt-5">
-                    Option 3: <Input />
+                    Option 3:
+                    <Input
+                        value={option3}
+                        onChange={(option3) => setOption3(option3.target.value)}
+                    />
                 </div>
 
                 <div className="pt-5">
-                    <Button>Submit Poll</Button>
+                    <Button onClick={handleClick}>
+                        Submit Poll
+                    </Button>
                 </div>
+
             </div>
         </div>
 
